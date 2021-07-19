b6119b7d3eb6dea4a08c6aacaaeafe9a30698888
shenghao <shenghao7777@qq.com>
2021-07-19 23:43:48 +0800

    2021.07.19提交

--- app/src/main/java/com/shenghao/todaynews/api/ApiRetrofit.java
+++ app/src/main/java/com/shenghao/todaynews/api/ApiRetrofit.java
@@ -1,7 +1,27 @@
 package com.shenghao.todaynews.api;
 
+import android.util.TimeUtils;
+
+import com.google.gson.GsonBuilder;
+import com.shenghao.todaynews.app.ApiConstant;
+import com.shenghao.todaynews.app.MyApp;
+import com.shenghao.todaynews.utils.NetWorkUtils;
+import com.socks.library.KLog;
+
+import java.io.File;
+import java.nio.channels.NetworkChannel;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class ApiRetrofit {
 
@@ -9,4 +29,103 @@ public class ApiRetrofit {
     private final Retrofit mRetrofit;
     private OkHttpClient mClient;
     private ApiService mApiService;
+
+    //缓存设置
+    private Interceptor mCacheInterceptor = chain -> {
+        CacheControl.Builder cacheBuilder = new CacheControl.Builder();
+        cacheBuilder.maxAge(0, TimeUnit.SECONDS);
+        cacheBuilder.maxStale(365, TimeUnit.DAYS);
+        CacheControl cacheControl = cacheBuilder.build();
+
+        Request request = chain.request();
+        if(!NetWorkUtils.isNetworkAvailable(MyApp.getmContext())){
+            request = request.newBuilder()
+                    .cacheControl(cacheControl)
+                    .build();
+        }
+        Response originalResponse = chain.proceed(request);
+        if(NetWorkUtils.isNetworkAvailable(MyApp.getmContext())){
+            int maxAge = 0;
+            return originalResponse.newBuilder()
+                    .removeHeader("Pragma")
+                    .header("Cache-Control", "public, max-age=" + maxAge)
+                    .build();
+        }else{
+            int maxStale = 60 * 60 * 24 * 28;
+            return originalResponse.newBuilder()
+                    .removeHeader("Pragma")
+                    .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                    .build();
+        }
+    };
+
+    /**
+     * 请求访问quest和response拦截器
+     */
+    private  Interceptor mLogInterceptor = chain -> {
+        Request request = chain.request();
+        long startTime = System.currentTimeMillis();
+        okhttp3.Response response = chain.proceed(chain.request());
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        okhttp3.MediaType mediaType = response.body().contentType();
+        String content = response.body().string();
+        KLog.e("----------Request Start----------------");
+        KLog.e("| " + request.toString());
+        KLog.json("| Response:" + content);
+        KLog.e("----------Request End:" + duration + "毫秒----------");
+        return response.newBuilder()
+                .body(okhttp3.ResponseBody.create(mediaType, content))
+                .build();
+    };
+
+    /**增加头部信息的拦截器*/
+    private Interceptor mHeaderInterceptor = chain -> {
+        Request.Builder builder = chain.request().newBuilder();
+        builder.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.108 Safari/537.36 2345Explorer/8.0.0.13547");
+        builder.addHeader("Cache-Control", "max-age=0");
+        builder.addHeader("Upgrade-Insecure-Requests", "1");
+        builder.addHeader("X-Requested-With", "XMLHttpRequest");
+        builder.addHeader("Cookie", "uuid=\"w:f2e0e469165542f8a3960f67cb354026\"; __tasessionId=4p6q77g6q1479458262778; csrftoken=7de2dd812d513441f85cf8272f015ce5; tt_webid=36385357187");
+        return chain.proceed(builder.build());
+    };
+
+    public ApiRetrofit(){
+        File httpCacheDirectory = new File(MyApp.getmContext().getCacheDir(), "responses");
+        int cacheSize = 10 * 1024 * 1024;
+        Cache cache = new Cache(httpCacheDirectory, cacheSize);
+
+        mClient = new OkHttpClient.Builder()
+                .addInterceptor(mHeaderInterceptor)
+                .addInterceptor(mLogInterceptor)
+                .cache(cache)
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .build();
+
+        mRetrofit = new Retrofit.Builder()
+                .baseUrl(ApiConstant.BASE_SERVER_URL)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().create()))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create()) //支持rxjava
+                .client(mClient)
+                .build();
+
+        mApiService = mRetrofit.create(ApiService.class);
+    }
+
+    public static ApiRetrofit getInstance(){
+        if(mApiRetrofit == null){
+            synchronized (Object.class){
+                if(mApiRetrofit == null){
+                    mApiRetrofit = new ApiRetrofit();
+                }
+            }
+        }
+        return mApiRetrofit;
+    }
+
+    public ApiService getmApiService(){
+        return mApiService;
+    }
 }
--- app/src/main/java/com/shenghao/todaynews/api/SubscriberCallBack.java
+++ app/src/main/java/com/shenghao/todaynews/api/SubscriberCallBack.java
@@ -1,6 +1,45 @@
 package com.shenghao.todaynews.api;
 
+import android.text.TextUtils;
+
+import com.shenghao.todaynews.model.response.ResultResponse;
+import com.shenghao.todaynews.utils.UIUtils;
+import com.socks.library.KLog;
+
 import rx.Subscriber;
 
-public class SubscriberCallBack extends Subscriber {
+/**
+ * 抽取callback
+ */
+public abstract class SubscriberCallBack<T> extends Subscriber<ResultResponse<T>> {
+
+    @Override
+    public void onNext(ResultResponse<T> tResultResponse) {
+        boolean isSuccess = (!TextUtils.isEmpty(tResultResponse.message) &&
+                tResultResponse.message.equals("success")) || !TextUtils.isEmpty(tResultResponse.success)
+                && tResultResponse.success.equals("true");
+        if(isSuccess){
+            onSuccess((T) tResultResponse.data);
+        }else{
+            UIUtils.showToast(tResultResponse.message);
+        }
+    }
+
+    @Override
+    public void onCompleted() {
+
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        KLog.e(e.getLocalizedMessage());
+        onError();
+    }
+
+    protected abstract void onSuccess(T response);
+    protected abstract void onError();
+
+    protected void onFailure(ResultResponse response){
+
+    }
 }
